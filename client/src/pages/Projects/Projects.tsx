8e7a40c272e719c3f623e414b9506c90a1ac8167
luisosta <luisosta@gmail.com>
2020-02-14 14:47:45 -0700

    added basic routing and pages

---
+++ client/src/pages/Projects/Projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Projects: React.FC = () => {
+
+    return <h1>Projects Page</h1>
+}
+
+
+export default Projects;
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Home from "./Home/Home";
+import Funding from './Funding/Funding'
+import Projects from './Projects/Projects'
+
+export {
+    Home,
+    Funding,
+    Projects
+}
