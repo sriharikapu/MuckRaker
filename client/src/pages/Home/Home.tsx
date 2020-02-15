d561fefe51d3ea4904a550937583dc5a007e6064
luisosta <luisosta@gmail.com>
2020-02-15 12:07:04 -0700

    redesigned header and home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,98 +1,60 @@
-import { Button, Grid, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
-import Title from './Title';
+import React from "react";
+import { Grid, TextField, Typography, InputAdornment } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        height: '100vh',
-        minHeight: '100vh'
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-        fontFamily: 'Raleway',
-        fontSize: 30,
-        fontWeight: 800
-    },
-    leftPaper: {
-        padding: 100,
-        background: 'linear-gradient(to right, rgb(250,250,250), rgb(220,220,220))'
-    },
-    rightPaper: {
-    },
-    control: {
-        padding: theme.spacing(2),
-    },
-    loginBtn: {
-        fontFamily: 'Raleway',
-    },
-    grid:
-    {
-        height: '100%'
-    },
-    appBar: {
-        backgroudColor: 'black'
-    },
-    primaryBtn: {
-        width: '100%',
-        fontFamily: 'Raleway',
-        fontSize: 20,
-        fontWeight: 500,
-        background: 'black'
-    }
-}));
-
-
-
-export default function ButtonAppBar() {
-    const classes = useStyles();
-
-    return (
-        <Grid
-            container
-            direction="row"
-            justify="space-evenly"
-            alignItems="center"
-            style={{ minHeight: '100vh' }}
+const useStyles = makeStyles({
+  root: {
+    paddingLeft: "5vw"
+  },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+  },
+  title: {
+    fontFamily: "Slabo"
+  }
+});
+export default function Home() {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={12} md={5} className={classes.container}>
+        <Typography
+          variant="h1"
+          component="h2"
+          gutterBottom
+          className={classes.title}
         >
-            <Grid
-                item
-                direction="column"
-                alignItems="center"
-                justify="center"
-                key={1}
-            >
-                <Paper className={classes.leftPaper}>
-                    <Title top='Muck' bottom="Rakers" fontSize={100} />
-                    <br />
-                    <br />
-                    <span style={{ fontFamily: 'Raleway', fontSize: 20 }}>
-                        Enabling journalists to report without fear
-                    </span>
-                </Paper>
-                <div style={{ height: 20 }} />
-                <Button variant="contained" color="primary" className={classes.primaryBtn} >
-                    Get Started
-            </Button>
-            </Grid>
-            <Grid
-                item
-                spacing={0}
-                direction="column"
-                alignItems="center"
-                justify="center"
-                key={2}
-            >
-                <img src="/images/press_freedom_fists.png" style={{
-                    borderRadius: 10,
-                    WebkitMaskImage: '-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))',
-                    maskImage: 'linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))'
-                }} />
-            </Grid>
-        </Grid >
-    );
+          Shine Light A On The Truth
+        </Typography>
+        <TextField
+          placeholder="Search Projects"
+          variant="outlined"
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchOutlined />
+              </InputAdornment>
+            )
+          }}
+        />
+      </Grid>
+      <Grid item xs={12} md={7} className={classes.container}>
+        <img
+          src="/images/press_freedom_fists.png"
+          style={{
+            borderRadius: 10,
+            WebkitMaskImage:
+              "-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))",
+            maskImage:
+              "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))"
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
 }
--- client/src/web3/index.ts
+++ client/src/web3/index.ts
@@ -1,5 +1,11 @@
 const Fortmatic = require('fortmatic')
 import Web3 from 'web3'
+declare global {
+    interface Window { ethereum: any; web3: any }
+}
+
+window.ethereum = window.ethereum || {};
+window.web3 = window.web3 || {};
 
 const fm = new Fortmatic('');
 
