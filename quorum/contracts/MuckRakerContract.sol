0313fa9a9af6f3d72fee1efa133cf3b149bcf4ae
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-02-15 17:55:04 -0700

    Added funding mappings

--- quorum/contracts/MuckRakerContract.sol
+++ quorum/contracts/MuckRakerContract.sol
@@ -9,16 +9,45 @@ contract MuckRakerContract {
     mapping(address => string[]) isFunding;
     mapping(string => address[]) isFundedBy;
 
-    function create_product(address ownerAddress, string memory productCID)
+    function create_project(address ownerAddress, string memory productCID)
         public
-        returns (bool success)
+        returns (uint256)
     {
         string[] storage products = ownsProducts[ownerAddress];
         products.push(productCID);
 
         isOwnedBy[productCID] = ownerAddress;
 
-        return true;
+        return products.length;
+    }
+
+    function fund_project(address funderAddress, string memory productCID)
+        public
+        returns (string memory)
+    {
+        string[] storage funding = isFunding[funderAddress];
+        funding.push(productCID);
+
+        address[] storage fundedBy = isFundedBy[productCID];
+        fundedBy.push(funderAddress);
+
+        return productCID;
+    }
+
+    function get_funders(string memory productCID)
+        public
+        view
+        returns (address[] memory)
+    {
+        return isFundedBy[productCID];
+    }
+
+    function get_funding(address funderAddress)
+        public
+        view
+        returns (string[] memory)
+    {
+        return isFunding[funderAddress];
     }
 
     function get_owner(string memory productCID) public view returns (address) {
